d4cbc4fec967ec1a14c57edc6e603ab490c16209
Samuel Wilton <Samuel.Wilton@ontario.ca>
2025-03-13 10:46:52 -0400

    Installed component lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   imports: [],
   template: `
-	<h1>Hello world</h1>
+	<h1>First Angular App</h1>
   `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-app';
 }
