d3fe807f7c715513342060d8e8a6fea6c36ae157
Samuel Wilton <Samuel.Wilton@ontario.ca>
2025-03-13 11:32:08 -0400

    setup backend server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,13 @@
 import express from "express"
 import cors from "cors"
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World from Node.js server!');
+  });
+
+app.listen(port, () => {
+	console.log("Website served on http://localhost:" + port);
+})
