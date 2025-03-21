ca1d28eed53a2382b4fcb9fabd3a4e0c61401088
Samuel Wilton <Samuel.Wilton@ontario.ca>
2025-03-21 16:32:25 -0400

    Refactored server to be more modular and easy to setup new get routes. Added new get routes to allow easier access to column names, schema, and primary key column.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,36 +3,76 @@ import express from "express"
 import cors from "cors"
 
 const app = express();
-app.use(cors());
 const port = 5000;
+const config: string = "server=ON44C03490451\\MSSQLSERVER01;Database=pubs;Trusted_Connection=true;Driver={SQL Server}"
 
-let config: string = "server=ON44C03490451\\MSSQLSERVER01;Database=pubs;Trusted_Connection=true;Driver={SQL Server}"
-
-app.get('/api/:table/:id?', (req, res) => {
-	sql.open(config, (err: MsNodeSqlV8.Error, conn: MsNodeSqlV8.Connection) => {
-		let table = req.params.table;
-		let id: string | undefined = req.params.id;
-		let querystr: string = `SELECT * FROM ${table}`;
-		conn.query(querystr, (err: any, result: any) => {
-			if (err) console.log(err);
-			else {
-				if (id) {
-					let idName: string = Object.keys(result[0])[0];
-					conn.query(`SELECT * FROM ${table} WHERE ${idName}='${id}'`, (err: any, details: any) => {
-						if (err) console.log(err);
-						else res.send(details);
-					})
-				}
-				else {
-					res.send(result);
-				}
-			}
-		})
-	});
-});
-
+app.use(cors());
 app.listen(port, () => {
 	console.log("Website served on http://localhost:" + port);
 });
+
+
+function queryDb(req: express.Request, res: express.Response, connErr: MsNodeSqlV8.Error, conn: MsNodeSqlV8.Connection, queryStr: string) {
+	if (connErr) {
+		console.log(connErr);
+		res.status(500).send("Could not open connection to SQL Server");
+		return;
+	}
+	conn.query(queryStr, (queryErr?: MsNodeSqlV8.Error, result?: { [index: string]: string }[]) => {
+		if (queryErr) {
+			console.log(queryErr);
+			res.status(500).send("Error executing query");
+		} else {
+			if (result!.length > 0) {
+				res.send(result);
+			} else {
+				res.status(404).send("No results found from query");
+			}
+		}
+	});
+
+}
+
+
+function sqlOpenWrapper(req: express.Request, res: express.Response, queryStr: string) {
+	sql.open(config, (err: MsNodeSqlV8.Error, conn: MsNodeSqlV8.Connection) => {
+		queryDb(req, res, err, conn, queryStr);
+	});
+}
+
+
+app.get('/api/:table/pk', (req: express.Request, res: express.Response) => {
+	let queryStr = `SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '${req.params.table}'`;
+	sqlOpenWrapper(req, res, queryStr);
+});
+
+
+app.get('/api/:table/schema', (req: express.Request, res: express.Response) => {
+	let queryStr = `
+	SELECT 
+		COLUMN_NAME, 
+		IS_NULLABLE, 
+		DATA_TYPE, 
+		CHARACTER_MAXIMUM_LENGTH, 
+		NUMERIC_PRECISION, 
+		NUMERIC_SCALE 
+	FROM INFORMATION_SCHEMA.COLUMNS 
+	WHERE TABLE_NAME = '${req.params.table}'
+	`
+	sqlOpenWrapper(req, res, queryStr);
+});
+
+
+app.get('/api/:table/:id?', async (req: express.Request, res: express.Response) => {
+	let queryStr: string = "";
+	if (req.params.id) {
+		let idName: string = await fetch("http://localhost:5000/api/authors/pk").then(response => response.json().then(pk => idName = pk[0]['COLUMN_NAME']));
+		queryStr = `SELECT * FROM ${req.params.table} WHERE ${idName}='${req.params.id}'`;
+	} else {
+		queryStr = `SELECT * FROM ${req.params.table}`;
+	}
+	sqlOpenWrapper(req, res, queryStr);
+});
+
 
 
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { DetailsComponent } from './details/details.component';
 
