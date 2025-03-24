8325b20d2a2feac6c703c6486cfc9f8b3650453b
Samuel Wilton <Samuel.Wilton@ontario.ca>
2025-03-24 16:04:52 -0400

    Added mat button, changed naming of variables to camel case. Edited table and details components to use fork joins.

--- frontend/src/app/details/details.component.ts
+++ frontend/src/app/details/details.component.ts
@@ -1,34 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableService } from '../table/table.service';
-import { PrimaryKey, Schema } from '../interfaces';
+import { Schema } from '../interfaces';
+import { forkJoin } from 'rxjs';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
 	@Input() id!: string;
-	table_name: string = 'authors';
-	table_headers!: string[];
+	tableName: string = 'authors';
+	tableHeaders!: string[];
 	record!: { [index: string]: string};
-	table_schema!: Schema[]
-	table_pkey!: string;
+	tableSchema!: Schema[]
+	tablePKey!: string;
 
 	constructor(private tblservice: TableService) {}
 
 	ngOnInit(): void {
-		this.tblservice.getRecord(this.table_name, this.id).subscribe((row: { [index: string]: string }) => {
-			this.record = row;
-			this.table_headers = Object.keys(row);
-		});
-		this.tblservice.getPkName(this.table_name).subscribe((data: PrimaryKey) => {
-			this.table_pkey = data.COLUMN_NAME;
-		});
-		this.tblservice.getSchema(this.table_name).subscribe((data: Schema[]) => {
-			this.table_schema = data;
+		forkJoin({
+			record: this.tblservice.getRecord(this.tableName, this.id),
+			pkey: this.tblservice.getPkName(this.tableName),
+			schema: this.tblservice.getSchema(this.tableName)
+		  }).subscribe(({ record, pkey, schema }) => {
+			this.record = record;
+			this.tableHeaders = Object.keys(record);
+			this.tablePKey = pkey.COLUMN_NAME;
+			this.tableSchema = schema;
 		});
 	}
 }
--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { TableService } from './table.service';
 import { RouterLink } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
 	selector: 'app-table',
@@ -10,20 +11,21 @@ import { RouterLink } from '@angular/router';
 	styleUrl: './table.component.css'
 })
 export class TableComponent {
-	table_name: string = 'authors';
-	table_rows!: { [index: string]: string }[];
-	table_headers!: string[];
-	table_pkey!: string;
+	tableName: string = 'authors';
+	tableRows!: { [index: string]: string }[];
+	tableHeaders!: string[];
+	tablePKey!: string;
 
 	constructor(private tblservice: TableService) {}
 
 	ngOnInit(): void {
-		this.tblservice.fetchTable(this.table_name).subscribe(rows => {
-			this.table_headers = Object.keys(rows[0]);
-			this.table_rows = rows;
-		});
-		this.tblservice.getPkName(this.table_name).subscribe(data => {
-			this.table_pkey = data['COLUMN_NAME'];
+		forkJoin({
+			rows: this.tblservice.fetchTable(this.tableName),
+			pkey: this.tblservice.getPkName(this.tableName)
+		}).subscribe(({rows, pkey}) => {
+			this.tableHeaders = Object.keys(rows[0]);
+			this.tableRows = rows;
+			this.tablePKey = pkey['COLUMN_NAME'];
 		});
 	}
 }
