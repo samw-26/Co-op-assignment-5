9f416db2138c0df28a8a006107128472dc7a0c20
Samuel Wilton <Samuel.Wilton@ontario.ca>
2025-03-27 11:00:17 -0400

    Added delete method to server. Finished escaping all sql queries.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,19 +17,24 @@ app.listen(port, () => {
 function queryDb(req: express.Request, res: express.Response, connErr: MsNodeSqlV8.Error, conn: MsNodeSqlV8.Connection, queryStr: string) {
 	if (connErr) {
 		console.log(connErr);
-		res.status(500).send("Could not open connection to SQL Server");
+		res.status(500).send("Could not open connection to SQL Server.");
 		return;
 	}
 
 	conn.query(queryStr, (queryErr?: MsNodeSqlV8.Error, result?: { [index: string]: string }[]) => {
 		if (queryErr) {
 			console.log(queryErr);
-			res.status(500).send("Error executing query");
-		} else {
-			if (result!.length > 0) {
+			res.status(500).send("Error executing query.");
+		} 
+		else {
+			if (req.method === "GET" && result!.length > 0) {
 				res.send(result);
-			} else {
-				res.status(404).send("No results found from query");
+			} 
+			else if (req.method !== "GET"){
+				res.send("Query executed.");
+			}
+			else {
+				res.status(404).send("No results found from query.");
 			}
 		}
 	});
@@ -37,50 +42,56 @@ function queryDb(req: express.Request, res: express.Response, connErr: MsNodeSql
 }
 
 
-function sqlOpenWrapper(req: express.Request, res: express.Response, queryStr: string) {
+function sqlQueryWrapper(req: express.Request, res: express.Response, queryStr: string) {
 	sql.open(config, (err: MsNodeSqlV8.Error, conn: MsNodeSqlV8.Connection) => {
 		queryDb(req, res, err, conn, queryStr);
 	});
 }
 
+async function getPrimaryKey(table: string) {
+	const response = await fetch(`http://localhost:5000/api/${table}/pk`);
+	const pk = await response.json();
+	return pk[0]['COLUMN_NAME'];
+}
+
 
 app.get('/api/:table/pk', (req: express.Request, res: express.Response) => {
-	let queryStr = `SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '${req.params.table}'`;
-	sqlOpenWrapper(req, res, queryStr);
+	let queryStr = SqlString.format("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = ?", [req.params.table]);
+	sqlQueryWrapper(req, res, queryStr);
 });
 
 
 app.get('/api/:table/schema', (req: express.Request, res: express.Response) => {
-	let queryStr = `
-	SELECT 
-		COLUMN_NAME, 
-		IS_NULLABLE, 
-		DATA_TYPE, 
-		CHARACTER_MAXIMUM_LENGTH, 
-		NUMERIC_PRECISION, 
-		NUMERIC_SCALE 
-	FROM INFORMATION_SCHEMA.COLUMNS 
-	WHERE TABLE_NAME = '${req.params.table}'
-	`
-	sqlOpenWrapper(req, res, queryStr);
+	let queryStr = SqlString.format(`
+		SELECT 
+			COLUMN_NAME, 
+			IS_NULLABLE, 
+			DATA_TYPE, 
+			CHARACTER_MAXIMUM_LENGTH, 
+			NUMERIC_PRECISION, 
+			NUMERIC_SCALE 
+		FROM INFORMATION_SCHEMA.COLUMNS 
+		WHERE TABLE_NAME = ?
+		`, [req.params.table])
+	sqlQueryWrapper(req, res, queryStr);
 });
 
 
 app.get('/api/:table/:id?', async (req: express.Request, res: express.Response) => {
 	let queryStr: string = "";
 	if (req.params.id) {
-		let idName: string = await fetch("http://localhost:5000/api/authors/pk").then(response => response.json().then(pk => idName = pk[0]['COLUMN_NAME']));
-		console.log(queryStr)
-		queryStr = SqlString.format("SELECT * FROM ?? WHERE au_id=?", [req.params.table, req.params.id]);
-		console.log(queryStr)
+		let idName: string = await getPrimaryKey(req.params.table);
+		queryStr = SqlString.format("SELECT * FROM ?? WHERE ??=?", [req.params.table, idName, req.params.id]);
 	} else {
-		queryStr = `SELECT * FROM ${req.params.table}`;
+		queryStr = SqlString.format("SELECT * FROM ??", [req.params.table]);
 	}
-	sqlOpenWrapper(req, res, queryStr);
+	sqlQueryWrapper(req, res, queryStr);
 });
 
-app.put('/api/:table/:pk/:id', (req: express.Request, res: express.Response) => {
-	let queryStr = `DELETE FROM ${req.params.table} WHERE ${req.params.pk} = ${req.params.id}`
+app.delete('/api/:table/:id', async (req: express.Request, res: express.Response) => {
+	let idName =  await getPrimaryKey(req.params.table);
+	let queryStr = SqlString.format("DELETE FROM ?? WHERE ?? = ?", [req.params.table, idName, req.params.id]);
+	sqlQueryWrapper(req, res, queryStr);
 });
 
 
